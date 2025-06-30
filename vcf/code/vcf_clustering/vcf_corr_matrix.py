7ae4cd8b6b10051c3fb916e8fc5a982d7eaaecab
Eric Xu <eiyxu03@gmail.com>
2025-06-30 15:05:04 -0400

    Add VCF binary conc. correlation matrix figures

--- vcf/code/vcf_clustering/vcf_corr_matrix.py
+++ vcf/code/vcf_clustering/vcf_corr_matrix.py
@@ -3,35 +3,35 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import pairwise_distances
-
-
-""" 
-text_embed_corr_matrix.py
-
-Script that obtains the correlation matrices for text embedding data. 
-
-Usage:
-    python text_embed_corr_matrix.py
+sys.path.append('../')
+from extract_binary import get_binary_with_threshold
 
 """
+vcf_corr_matrix.py
+
+Script that obtains the correlation matrix for the binary VCF dataset.
+
+Usage: 
+    python vcf_corr_matrix.py
+"""
+
 
 # ===== PROGRAM SPECIFICATIONS =====
 store_results = True
-output_folder = "../figures/corr_matrix_figures"
+input_path = "../../Matrix.csv"
+foods_path = "../../edited_category_data.json"
+output_folder = "../../figures/corr_matrix_figures"
+threshold = 1
 
 
 # ===== DATA RETRIEVAL =====
-word_embeddings_df = pd.read_csv("./output_word_embeddings.csv")
-smellword_embeddings_df = pd.read_csv("./output_smellword_embeddings.csv")
-pca_reduced_binary_df = pd.read_csv("../code/vcf_clustering/pca_reduced_binary.csv")
-word_array = word_embeddings_df.T.to_numpy()
-smellword_array = smellword_embeddings_df.T.to_numpy()
-pca_array = pca_reduced_binary_df.to_numpy()
+vcf_df, combined_indices, combined_names = get_binary_with_threshold(input_path, foods_path, "ALL", threshold)
+vcf_array = vcf_df.to_numpy()
 
 
 # ===== POPULATING CORRELATION MATRIX =====
-array_of_choice = word_array
-metric = "euclidean"
+array_of_choice = vcf_array
+metric = "hamming"
 corr_matrix = np.zeros((array_of_choice.shape[0], array_of_choice.shape[0]))
 for idx1 in range(array_of_choice.shape[0]-1):
     for idx2 in range(idx1+1, array_of_choice.shape[0]):
@@ -45,7 +45,7 @@ fig, ax = plt.subplots()
 im = ax.imshow(corr_matrix, cmap='viridis', origin='lower', interpolation='nearest')
 cbar = fig.colorbar(im, ax=ax)
 cbar.set_label('Value Range')
-ax.set_title(f'Food Word Embeddings Correlation Matrix w/ {metric} metric')
+ax.set_title(f'VCF Binary Correlation Matrix w/ {metric} metric')
 if store_results:
-    plt.savefig(f"{output_folder}/foodword_embedding_metric={metric}_corr_matrix.pdf")
+    plt.savefig(f"{output_folder}/binary_VCF_metric={metric}_corr_matrix.pdf")
 plt.show()
